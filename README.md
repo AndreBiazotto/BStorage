282d1b8897b079c0b24d021be59e0f1560b917ab
AndreBiazotto <andrerafael.sbiazotto@gmail.com>
2025-10-21 21:42:12 -0300

    Versão alfa 0.0.2

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Versão alfa 0.0.1
 Nessa versão os dados são salvos em listas em arquivos de texto.
 Só é possível salvar e excluir
 Todo o tempo (entrega e devolução) e marcado automaticamente.
+
+Versão alfa 0.0.2
+Metodo que separa cada linha das listas txt em uma nova listas próprias
+Metodo de visualização mais bonito.
--- main.py
+++ main.py
@@ -1,31 +1,35 @@
-
 from datetime import datetime
 
+def txt_to_list(filename):
+    items = []
+    with open(filename, 'r') as file:
+        for x in file:
+            items.append(x.replace('\n', '').split(';'))
+    return items
 
-emprestados = []
-with open('emprestados.txt', 'r') as emprestados_file:
-    for x in emprestados_file:
-        emprestados.append(x.replace('\n', ''))
+def printt(table):
+    atributes = [0 for _ in range(len(table[0]))]
+    for i in range(len(table)):
+        for j in range(len(table[i])):
+            if len(table[i][j]) > atributes[j]:
+                atributes[j] = len(table[i][j])
+    
+    for i in range(len(table)):
+        print(i + 1, end=' |')
+        for j in range(len(table[i])):
+            print(f" {table[i][j]}".ljust(atributes[j] + 2), end='|')
+        print()
 
-devolvidos = []
-with open('devolvidos.txt', 'r') as devolvidos_file:
-    for x in devolvidos_file:
-        devolvidos.append(x.replace('\n', ''))
-
+emprestados = txt_to_list('emprestados.txt')
+devolvidos = txt_to_list('devolvidos.txt')
 
 while True:
     
     print("últimos itens emprestados")
-    for i, item in enumerate(emprestados[::-1]):
-        if i >= 5:
-            break
-        print(f"{i + 1} - {item}")
+    printt(emprestados[::-1][0:5])
 
     print("\núltimos itens devolvidos")
-    for i, item in enumerate(devolvidos[::-1]):
-        if i >= 5:
-            break
-        print(f"{i + 1} - {item}")
+    printt(devolvidos[::-1][0:5])
 
     print("\n0 - SAIR | 1 - Novo Empréstimo | 2 - Nova Devolução")
     opcoes = int(input("Escolha uma opção: "))
@@ -37,20 +41,29 @@ while True:
             item = input("Nome do item: ")
             pessoa = input("Nome da pessoa: ")
             data = datetime.now().strftime("%d/%m/%Y %H:%M")
-            emprestados.append(f'"{item}";"{pessoa}";"{data}"')
+
+            emprestados.append([item, pessoa, data])
+
             with open('emprestados.txt', 'a') as emprestados_file:
                 emprestados_file.write(f'"{item}";"{pessoa}";"{data}"\n')
+
             print("Empréstimo registrado com sucesso!")
         case 2:
             for i, item in enumerate(emprestados):
                 print(f"{i + 1} - {item}")
+
             devolucao_index = int(input("Escolha o número do item devolvido: ")) 
             devolvido_item = emprestados.pop(devolucao_index - 1)
-            devolvido_item += f';"{datetime.now().strftime("%d/%m/%Y %H:%M")}"'
+            devolvido_item.append(f'"{datetime.now().strftime("%d/%m/%Y %H:%M")}"')
             devolvidos.append(devolvido_item)
+
             with open('devolvidos.txt', 'a') as devolvidos_file:
-                devolvidos_file.write(f"{devolvido_item}\n")
+                aux = ';'.join(devolvido_item)
+                devolvidos_file.write(f"{aux}\n")
+
             with open('emprestados.txt', 'w') as emprestados_file:
                 for item in emprestados:
-                    emprestados_file.write(f"{item}\n")
+                    aux = ';'.join(item)
+                    emprestados_file.write(f"{aux}\n")
+
             print("Devolução registrada com sucesso!")
